1813a6310a4d1aff5a2d2d665a9e0a5e58e4609e
Vaishnavi <vishumitl24@gmail.com>
2025-02-05 09:56:23 +0530

    some more

--- CE/file1.js
+++ CE/file1.js
@@ -1,6 +1,6 @@
 const fs = require('fs'); //fs=file system
 
-//const data=fs.readFileSync("./data.txt","utf-8");
+const data=fs.readFileSync("./data.txt","utf-8");
 //or
 //const data=fs.readFileSync("./data.txt",{encoding:"utf-8"});
 // console.log(data.toString());
@@ -9,4 +9,13 @@ fs.writeFileSync("./data1.txt","so,this is my data and where is your data");
 
 fs.appendFileSync("./data1.txt","here i am ");
 fs.renameSync("./data1.txt","./data2.txt")
+
+//to delete the file
+fs.unlinkSync("./data2.txt");
+
+if(data.match("h")){
+    console.log("h is present");
+    const changeData= data.replace("h","hhhhhhhhhhh");
+    fs.writeFileSync("./data.txt",changeData);
+}
 
